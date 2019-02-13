2822637e38aba7ae9e92dcc27932c43a296fda8a
duongkh <jerryduongzz1999@gmail.com>
2019-02-13 16:18:08 +0000

    add database

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ console.log('index.js executing');
 
 var express = require('express');
 var app = express();
+var addOne = require('./myModule');
+
+addOne([1,3,0]);
+
 app.get( '/', function(req,res){
 	res.send('Hello, World!');
 });
 
-varport = 3000;
+var port = 3000;
 app.listen(port,function(){
 	console.log('Listening on port ' + port);
 });
+
+var router = express.Router();
+
+router.get('/', function(req,res){ res.send('Index')});
+router.post('/', function(req, res){ res.send('About Index')});
+
+app.use('/', router);
+
